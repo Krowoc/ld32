fd40a412c62270030cf854e21b8056bba9efefb5
Adam Silcott <adsilcott@gmail.com>
2015-04-18 15:41:10 -0400

    Added jumping to the character todo: prevent multi-jumping

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,16 +5,28 @@ public class Character : MonoBehaviour {
 
 	public float mood;
 
+	public float jumpForce = 1.0f;
+	public float currentJumpForce;
+	public float gravity = -0.3f;
+	public bool jumping = false;
+	public float groundPosition;
+
 	public Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator>();
+
+		groundPosition = transform.position.y;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		if(Input.GetButtonDown("Jump"))
+		{
+			StartCoroutine ("Jump");
+		}
 
 		if(Input.GetKey (KeyCode.T))
 		{
@@ -38,4 +50,35 @@ public class Character : MonoBehaviour {
 			mood += wo.damage;
 		}
 	}
+
+	IEnumerator Jump()
+	{
+		currentJumpForce = jumpForce;
+
+		Vector3 pos = transform.position;
+		
+		pos += new Vector3(0.0f, currentJumpForce);
+		
+		transform.position = pos;
+		
+
+		while(transform.position.y > groundPosition)
+		{
+			yield return null;
+
+			currentJumpForce -= gravity;
+
+			Vector3 pos2 = transform.position;
+
+			pos2 += new Vector3(0.0f, currentJumpForce);
+
+			transform.position = pos2;
+
+			if(transform.position.y < groundPosition)
+				transform.position = new Vector3(transform.position.x, groundPosition);
+		}
+
+	}
+
+
 }
