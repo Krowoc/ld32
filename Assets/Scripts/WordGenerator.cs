3597de41bf326531628f758ee762fe3441eb51bc
Adam Silcott <adsilcott@gmail.com>
2015-04-19 16:28:59 -0400

    Fixed jumping bug, added file parsing method

--- Assets/Scripts/WordGenerator.cs
+++ Assets/Scripts/WordGenerator.cs
@@ -19,7 +19,7 @@ public class WordGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () 
 	{
-		wordList = new List<Words>();
+		/*wordList = new List<Words>();
 
 		string[] lineArray = wordFile.text.Split(new string[] { "\r\n", "\n" }, System.StringSplitOptions.None);//","[0]);
 
@@ -28,8 +28,9 @@ public class WordGenerator : MonoBehaviour {
 			string[] wordArray = lineArray[i].Split (new string[] { "," }, System.StringSplitOptions.None);
 
 			wordList.Add (new Words(wordArray[0], float.Parse (wordArray[1]), wordArray[2], Int32.Parse (wordArray[3])));
-		}
+		}*/
 
+		wordList = ParseWordFile(wordFile);
 
 		StartCoroutine ("SpawnWord");
 
@@ -44,27 +45,37 @@ public class WordGenerator : MonoBehaviour {
 	IEnumerator SpawnWord() 
 	{
 
-		if(Time.timeScale == 0.0000001f)
-			yield return null;
-		else
-
-			while(true)
-			{
-				int rand = Mathf.FloorToInt (UnityEngine.Random.Range(0, wordList.Count));
-				
-				GameObject wordGO = Instantiate (Resources.Load<GameObject>("Word")) as GameObject;
-				
-				WordObject word = wordGO.GetComponent<WordObject>();
-				
-				word.SetMovementVector (new Vector3(-movementSpeed, 0.0f));
-				word.SetWord(wordList[rand].word, wordList[rand].damage, wordList[rand].power);
-				
-				float randHeight = UnityEngine.Random.Range (wordHeightMin, wordHeightMax);
-				word.transform.position = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width, Screen.height * randHeight, -Camera.main.transform.position.z) );
-				
-				yield return new WaitForSeconds(spawnSpeed);
-			}
+		while(true)
+		{
+			int rand = Mathf.FloorToInt (UnityEngine.Random.Range(0, wordList.Count));
+			
+			GameObject wordGO = Instantiate (Resources.Load<GameObject>("Word")) as GameObject;
+			
+			WordObject word = wordGO.GetComponent<WordObject>();
+			
+			word.SetMovementVector (new Vector3(-movementSpeed, 0.0f));
+			word.SetWord(wordList[rand].word, wordList[rand].damage, wordList[rand].power);
+			
+			float randHeight = UnityEngine.Random.Range (wordHeightMin, wordHeightMax);
+			word.transform.position = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width, Screen.height * randHeight, -Camera.main.transform.position.z) );
+			
+			yield return new WaitForSeconds(spawnSpeed);
+		}
 	}
 
+	public List<Words> ParseWordFile(TextAsset wFile)
+	{
+		List<Words> wList = new List<Words>();
+		
+		string[] lineArray = wFile.text.Split(new string[] { "\r\n", "\n" }, System.StringSplitOptions.None);//","[0]);
+		
+		for(int i = 1; i < lineArray.Length; i++)
+		{
+			string[] wordArray = lineArray[i].Split (new string[] { "," }, System.StringSplitOptions.None);
+			
+			wList.Add (new Words(wordArray[0], float.Parse (wordArray[1]), wordArray[2], Int32.Parse (wordArray[3])));
+		}
 
+		return wList;
+	}
 }
