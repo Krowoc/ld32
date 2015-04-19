0fc5ed7e90d67d071dca83cc04ab1a00d6acf395
Adam Silcott <adsilcott@gmail.com>
2015-04-18 22:51:56 -0400

    Words are now loaded from a text file .csv

---
+++ Assets/Scripts/Words.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Words {
+
+	public string word;
+	public float damage;
+	public string power;
+	public int level;
+
+	public Words(string w, float d, string p, int l)
+	{
+		word = w;
+		damage = d;
+		power = p;
+		level = l;
+	}
+}
