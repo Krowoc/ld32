1b65ffccf4c6a2799cbc660499453225a4ce4401
Adam Silcott <adsilcott@gmail.com>
2015-04-25 14:02:09 -0400

    Added persistent Hi Score. Updated credits to remove unused song and add bfxr for hit sound effect

--- Assets/Scripts/GameOverMenu.cs
+++ Assets/Scripts/GameOverMenu.cs
@@ -4,16 +4,41 @@ using System.Collections;
 
 public class GameOverMenu : MonoBehaviour {
 
-	public Text score;
+	public Text scoreText;
+	public Text hiScoreText;
+	float score;
+	float hiScore;
+
 	// Use this for initialization
 	void Start () {
-		score = GameObject.Find ("Score").GetComponent<Text>();
-		score.text = (Globals.playerScore * 10).ToString ();
+
+		//Get Score and hi score
+		score = Globals.playerScore * 10;
+		hiScore = PlayerPrefs.GetFloat ("HiScore", score);
+
+		//Update hi score if needed
+		if(score > hiScore)
+		{
+			hiScore = score;
+			PlayerPrefs.SetFloat ("HiScore", hiScore);
+		}
+
+		//Display scores
+		scoreText = GameObject.Find ("Score").GetComponent<Text>();
+		scoreText.text = score.ToString ();
+
+		hiScoreText = GameObject.Find ("HiScore").GetComponent<Text>();
+		hiScoreText.text = hiScore.ToString ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//This is for testing the hi score
+		if(Input.GetKeyDown (KeyCode.R))
+		{
+			PlayerPrefs.DeleteKey ("HiScore");
+			Debug.Log ("Hi Score Reset");
+		}
 	}
 
 	public void OnClickReturnToMainMenu()
